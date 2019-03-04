cdb775bfd54ae57d4ee2d76fd818f92af0b0a1b4
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-04 15:06:48 +0200

    +sponsor api done+exhibtors api done+0.5details exhibors

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/fragment/DetailsExhibtorsFragment.java
@@ -0,0 +1,113 @@
+package com.example.alshimaa.exhibtion.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.example.alshimaa.exhibtion.NetworkConnection;
+import com.example.alshimaa.exhibtion.R;
+import com.example.alshimaa.exhibtion.YoutubeConfig;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailsExhibtorsFragment extends Fragment implements YouTubePlayer.OnInitializedListener {
+
+    String Link,Title,Address,ID;
+    TextView title,address;
+
+    NetworkConnection networkConnection;
+
+    public static final int RECOVERY_DIALOG_REQUEST=1;
+    private YouTubePlayerSupportFragment youTubePlayerSupportFragment;
+
+    TextView textToolbar;
+
+    public DetailsExhibtorsFragment() {
+        // Required empty public constructor
+    }
+
+View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view= inflater.inflate(R.layout.fragment_details_exhibtors, container, false);
+        init();
+        networkConnection=new NetworkConnection(getContext());
+        youTubePlayerSupportFragment.initialize(YoutubeConfig.DEVELOPER_KEY, this);
+
+        Bundle bundle=this.getArguments();
+        if (bundle!=null)
+        {
+            Link = bundle.getString( "video_link_exhibtor" );
+            Title=bundle.getString("title_exhibtor");
+            Address=bundle.getString("address_exhibtor");
+            ID=bundle.getString("id");
+
+            title.setText(Title);
+            address.setText(Address);
+            //  Toast.makeText(getContext(), ID, Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        return view;
+    }
+
+    private void init() {
+        title=view.findViewById(R.id.details_exhibtors_title);
+        address=view.findViewById(R.id.details_exhibtors_address);
+        textToolbar=view.findViewById(R.id.details_exhibtors_text_toolbar);
+        youTubePlayerSupportFragment = (YouTubePlayerSupportFragment)
+                getChildFragmentManager()
+                        .findFragmentById(R.id.details_exhibtors_youtube_player_support_fragment);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity(). getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if (!b) {
+
+            // loadVideo() will auto play video
+            // Use cueVideo() method, if you don't want to play it automatically
+             String url=Link.substring( Link.lastIndexOf( "=")+1  );
+            youTubePlayer.loadVideo(url);
+
+
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(getActivity(), RECOVERY_DIALOG_REQUEST).show();
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        getYouTubePlayerProvider().initialize(YoutubeConfig.DEVELOPER_KEY, this);
+    }
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerSupportFragment)getActivity()
+                .getSupportFragmentManager(). findFragmentById(R.id.details_exhibtors_youtube_player_support_fragment);
+    }
+}
+
---
+++ app/src/main/java/com/example/alshimaa/exhibtion/view/DetailsExhibtorsView.java
@@ -0,0 +1,8 @@
+package com.example.alshimaa.exhibtion.view;
+
+import com.example.alshimaa.exhibtion.model.ExhibtorsData;
+
+public interface DetailsExhibtorsView {
+    void showDetailsExhibtorsData(ExhibtorsData exhibtorsData);
+    void showError();
+}
