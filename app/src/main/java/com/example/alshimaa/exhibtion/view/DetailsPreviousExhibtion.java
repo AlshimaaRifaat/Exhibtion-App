fea6d6b8f01c9cc63c6e66b6661a23e7d257145f
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-07 14:09:04 +0200

    +design done+2+search prev exhib+details prev

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/view/DetailsPreviousExhibtion.java
@@ -0,0 +1,8 @@
+package com.example.alshimaa.exhibtion.view;
+
+import com.example.alshimaa.exhibtion.model.CurrentExhibtionData;
+import com.example.alshimaa.exhibtion.model.PreviousExhibtionData;
+
+public interface DetailsPreviousExhibtion {
+    void showExhibtionDetails(PreviousExhibtionData previousExhibtionData);
+}
