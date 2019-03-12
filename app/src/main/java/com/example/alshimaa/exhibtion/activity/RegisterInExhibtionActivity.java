9f00d09ac7b2ee377983765bfe6c32722bba89a3
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-12 12:51:20 +0200

    +add icon contact+icon under construct+1

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/activity/RegisterInExhibtionActivity.java
@@ -0,0 +1,15 @@
+package com.example.alshimaa.exhibtion.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alshimaa.exhibtion.R;
+
+public class RegisterInExhibtionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_in_exhibtion);
+    }
+}
