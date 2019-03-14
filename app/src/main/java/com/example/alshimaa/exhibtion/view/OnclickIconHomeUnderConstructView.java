084ab181570e2e6699673131d55d7093913ceab1
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-14 16:48:48 +0200

    nav service provider api done+visitor api done

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/view/OnclickIconHomeUnderConstructView.java
@@ -0,0 +1,7 @@
+package com.example.alshimaa.exhibtion.view;
+
+import com.example.alshimaa.exhibtion.model.HomeUnderConstructData;
+
+public interface OnclickIconHomeUnderConstructView {
+    void showOnclickIconHomeUnderConstructDetails(HomeUnderConstructData homeUnderConstructData);
+}
