e4c55a508dbb5438b9e4cd41952f5b0ed623dc08
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-14 12:14:40 +0200

    nav service provider api done

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/model/NavigationServiceProviderResponse.java
@@ -0,0 +1,83 @@
+package com.example.alshimaa.exhibtion.model;
+
+import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class NavigationServiceProviderResponse implements Serializable, Parcelable
+{
+
+    @SerializedName("data")
+    @Expose
+    private String data;
+    @SerializedName("status")
+    @Expose
+    private boolean status;
+    @SerializedName("error")
+    @Expose
+    private String error;
+    public final static Parcelable.Creator<NavigationServiceProviderResponse> CREATOR = new Creator<NavigationServiceProviderResponse>() {
+
+
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public NavigationServiceProviderResponse createFromParcel(Parcel in) {
+            return new NavigationServiceProviderResponse(in);
+        }
+
+        public NavigationServiceProviderResponse[] newArray(int size) {
+            return (new NavigationServiceProviderResponse[size]);
+        }
+
+    }
+            ;
+    private final static long serialVersionUID = -2796038859140654442L;
+
+    protected NavigationServiceProviderResponse(Parcel in) {
+        this.data = ((String) in.readValue((String.class.getClassLoader())));
+        this.status = ((boolean) in.readValue((boolean.class.getClassLoader())));
+        this.error = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    public NavigationServiceProviderResponse() {
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(data);
+        dest.writeValue(status);
+        dest.writeValue(error);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/example/alshimaa/exhibtion/view/NavigationServiceProviderView.java
@@ -0,0 +1,6 @@
+package com.example.alshimaa.exhibtion.view;
+
+public interface NavigationServiceProviderView {
+    void showNavServiceProviderData(String Msg);
+    void showError();
+}
