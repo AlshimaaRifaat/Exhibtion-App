c4d488bf28ba85b33040b23c61598d410176d3c0
Alshimaa <alshimaa.rifaat@gmail.com>
2019-03-20 15:32:10 +0200

    all app done+home service api done+details under construct

---
+++ app/src/main/java/com/example/alshimaa/exhibtion/view/DetailsExhibtionUnderConstructView.java
@@ -0,0 +1,8 @@
+package com.example.alshimaa.exhibtion.view;
+
+import com.example.alshimaa.exhibtion.model.HomeUnderConstructData;
+import com.example.alshimaa.exhibtion.model.PreviousExhibtionData;
+
+public interface DetailsExhibtionUnderConstructView {
+    void showDetailsExhibtionUnderConstruct(HomeUnderConstructData homeUnderConstructData);
+}
